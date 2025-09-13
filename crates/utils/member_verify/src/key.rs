86775e86c95dfe29a86da4bd34df94e5f03c795f
魏曹先生 <1992414357@qq.com>
2025-09-13 20:37:52 +0800

    Update `member_verify` modules

---
+++ crates/utils/member_verify/src/key.rs
@@ -0,0 +1,9 @@
+pub struct PrivateKey {
+    raw_key: String,
+    passphrase: String,
+}
+
+pub struct PublicKey {
+    raw_key: String,
+    fingerprint: String,
+}
---
+++ crates/utils/member_verify/src/verifier.rs
@@ -0,0 +1,10 @@
+
+
+#[cfg(test)]
+mod test_member_verify {
+
+    #[test]
+    fn test_verify() {
+
+    }
+}
