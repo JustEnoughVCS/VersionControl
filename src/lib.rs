f01c40cda780cbf84aed920efe251a9ac1f65839
魏曹先生 <1992414357@qq.com>
2025-09-22 10:31:10 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,29 @@
-// Feature `cfg_file`
-#[cfg(feature = "cfg_file")]
-pub mod cfg_file {
-    extern crate cfg_file;
-    pub use cfg_file::*;
-}
-
-// Feature `tcp_connection`
-#[cfg(feature = "tcp_connection")]
-pub mod tcp_connection {
-    extern crate tcp_connection;
-    pub use tcp_connection::*;
-}
-
-// Feature `string_proc`
-#[cfg(feature = "string_proc")]
-pub mod string_proc {
-    extern crate string_proc;
-    pub use string_proc::*;
-}
-
 // Feature `env`
 #[cfg(feature = "env")]
 pub mod env {
     extern crate env;
     pub use env::*;
+}
+
+pub mod utils {
+    // Feature `tcp_connection`
+    #[cfg(feature = "tcp_connection")]
+    pub mod tcp_connection {
+        extern crate tcp_connection;
+        pub use tcp_connection::*;
+    }
+
+    // Feature `string_proc`
+    #[cfg(feature = "string_proc")]
+    pub mod string_proc {
+        extern crate string_proc;
+        pub use string_proc::*;
+    }
+
+    // Feature `cfg_file`
+    #[cfg(feature = "cfg_file")]
+    pub mod cfg_file {
+        extern crate cfg_file;
+        pub use cfg_file::*;
+    }
 }
