9a99827af4695eee77a3ece7e4138c46a493fc73
魏曹先生 <1992414357@qq.com>
2025-09-22 16:50:38 +0800

    Update constants.rs

--- crates/env/src/constants.rs
+++ crates/env/src/constants.rs
@@ -1,3 +1,6 @@
+// -------------------------------------------------------------------------------------
+//
+
 // Project
 pub const PATH_TEMP: &str = "./.temp/";
 
@@ -13,26 +16,34 @@ pub const SERVER_PATH_SHEETS: &str = "./sheets/";
 pub const SERVER_FILE_SHEET: &str = "./sheets/{sheet-name}.yaml";
 
 // Server - Members
-pub const SERVER_PATH_MEMBER: &str = "./members/";
-pub const SERVER_FILE_MEMBER_INFO: &str = "./members/{member_uuid}.toml"; // crates::env::member::manager
-pub const SERVER_FILE_MEMBER_PUB: &str = "./key/{member_uuid}.pem"; // crates::utils::tcp_connection::instance
+pub const SERVER_PATH_MEMBERS: &str = "./members/";
+pub const SERVER_PATH_MEMBER_PUB: &str = "./key/";
+pub const SERVER_FILE_MEMBER_INFO: &str = "./members/{member_id}.toml"; // crates::env::member::manager
+pub const SERVER_FILE_MEMBER_PUB: &str = "./key/{member_id}.pem"; // crates::utils::tcp_connection::instance
 
-// Server - Storage
-pub const SERVER_PATH_VISUAL_FILE: &str = "./storage/";
-pub const SERVER_FILE_STORGAE_CONFIG: &str = "./storage.yaml";
+// Server - Virtual File Storage
+pub const SERVER_PATH_VIRTUAL_FILE_TEMP: &str = "./.temp/{temp_name}";
+pub const SERVER_PATH_VIRTUAL_FILE_ROOT: &str = "./storage/";
+pub const SERVER_PATH_VIRTUAL_FILE_STORAGE: &str = "./storage/{vf_id}/";
+pub const SERVER_FILE_VIRTUAL_FILE_VERSION_INSTANCE: &str = "./storage/{vf_id}/{vf_version}.rf";
+pub const SERVER_FILE_VIRTUAL_FILE_META: &str = "./storage/{vf_id}/meta.toml";
+
+pub const SERVER_FILE_README: &str = "./README.md";
 
 // -------------------------------------------------------------------------------------
 
 // Client
-pub const CLIENT_PATH_WORKSPACE_ROOT: &str = "./.jvc/";
+pub const CLIENT_PATH_WORKSPACE_ROOT: &str = "./.jv/";
 
 // Client - Workspace (Main)
-pub const CLIENT_FILE_WORKSPACE: &str = "./.jvc/workspace.toml"; // crates::env::local::local_config
+pub const CLIENT_FILE_WORKSPACE: &str = "./.jv/workspace.toml"; // crates::env::local::local_config
 
 // Client - Other
 pub const CLIENT_FILE_IGNOREFILES: &str = ".jgnore .gitignore"; // Support gitignore file.
 
+// -------------------------------------------------------------------------------------
+
 // User - Verify (System path)
-pub const USER_FILE_KEY: &str = "./.jvc_user/key";
-pub const USER_FILE_KEY_PUB: &str = "./.jvc_user/key.pub";
-pub const USER_FILE_MEMBER: &str = "./.jvc_user/self.toml";
+pub const USER_FILE_KEY: &str = "./.jv_user/key";
+pub const USER_FILE_KEY_PUB: &str = "./.jv_user/key.pub";
+pub const USER_FILE_MEMBER: &str = "./.jv_user/self.toml";
