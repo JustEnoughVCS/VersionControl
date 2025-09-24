08a579eac6a93489c52058b32a0fd9f00a8fb8d1
魏曹先生 <1992414357@qq.com>
2025-09-24 18:15:50 +0800

    Update member.rs

--- crates/env/src/workspace/vault/member.rs
+++ crates/env/src/workspace/vault/member.rs
@@ -19,7 +19,7 @@ const ID_PARAM: &str = "{member_id}";
 /// Member Manage
 impl Vault {
     /// Read member from configuration file
-    pub async fn member(&self, id: MemberId) -> Result<Member, std::io::Error> {
+    pub async fn member(&self, id: &MemberId) -> Result<Member, std::io::Error> {
         if let Some(cfg_file) = self.member_cfg(id) {
             let member = Member::read_from(cfg_file).await?;
             return Ok(member);
@@ -31,8 +31,8 @@ impl Vault {
     /// Update member info
     pub async fn update_member(&self, member: Member) -> Result<(), std::io::Error> {
         // Ensure member exist
-        if let Some(_) = self.member_cfg(member.id()) {
-            let member_cfg_path = self.member_cfg_path(member.id());
+        if let Some(_) = self.member_cfg(&member.id()) {
+            let member_cfg_path = self.member_cfg_path(&member.id());
             Member::write_to(&member, member_cfg_path).await?;
             return Ok(());
         }
@@ -43,7 +43,7 @@ impl Vault {
     /// Register a member to vault
     pub async fn register_member_to_vault(&self, member: Member) -> Result<(), std::io::Error> {
         // Ensure member not exist
-        if let Some(_) = self.member_cfg(member.id()) {
+        if let Some(_) = self.member_cfg(&member.id()) {
             return Err(Error::new(
                 ErrorKind::DirectoryNotEmpty,
                 format!("Member `{}` already registered!", member.id()),
@@ -51,14 +51,14 @@ impl Vault {
         }
 
         // Wrtie config file to member dir
-        let member_cfg_path = self.member_cfg_path(member.id());
+        let member_cfg_path = self.member_cfg_path(&member.id());
         Member::write_to(&member, member_cfg_path).await?;
 
         Ok(())
     }
 
     /// Remove member from vault
-    pub fn remove_member_from_vault(&self, id: MemberId) -> Result<(), std::io::Error> {
+    pub fn remove_member_from_vault(&self, id: &MemberId) -> Result<(), std::io::Error> {
         // Ensure member exist
         if let Some(member_cfg_path) = self.member_cfg(id) {
             fs::remove_file(member_cfg_path)?;
@@ -68,7 +68,7 @@ impl Vault {
     }
 
     /// Try to get the member's configuration file to determine if the member exists
-    pub fn member_cfg(&self, id: MemberId) -> Option<PathBuf> {
+    pub fn member_cfg(&self, id: &MemberId) -> Option<PathBuf> {
         let cfg_file = self.member_cfg_path(id);
         if cfg_file.exists() {
             Some(cfg_file)
@@ -78,7 +78,7 @@ impl Vault {
     }
 
     /// Try to get the member's public key file to determine if the member has login permission
-    pub fn member_key(&self, id: MemberId) -> Option<PathBuf> {
+    pub fn member_key(&self, id: &MemberId) -> Option<PathBuf> {
         let key_file = self.member_key_path(id);
         if key_file.exists() {
             Some(key_file)
@@ -88,7 +88,7 @@ impl Vault {
     }
 
     /// Get the member's configuration file path, but do not check if the file exists
-    pub fn member_cfg_path(&self, id: MemberId) -> PathBuf {
+    pub fn member_cfg_path(&self, id: &MemberId) -> PathBuf {
         let path = self
             .vault_path
             .join(SERVER_FILE_MEMBER_INFO.replace(ID_PARAM, id.to_string().as_str()));
@@ -96,7 +96,7 @@ impl Vault {
     }
 
     /// Get the member's public key file path, but do not check if the file exists
-    pub fn member_key_path(&self, id: MemberId) -> PathBuf {
+    pub fn member_key_path(&self, id: &MemberId) -> PathBuf {
         let path = self
             .vault_path
             .join(SERVER_FILE_MEMBER_PUB.replace(ID_PARAM, id.to_string().as_str()));
