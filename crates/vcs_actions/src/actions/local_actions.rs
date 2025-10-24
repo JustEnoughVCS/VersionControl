ac71a819dd45090a2ee1054208fd027f05a8c36c
魏曹先生 <1992414357@qq.com>
2025-10-24 18:24:04 +0800

    Fix some spelling issues.

--- crates/vcs_actions/src/actions/local_actions.rs
+++ crates/vcs_actions/src/actions/local_actions.rs
@@ -29,7 +29,24 @@ pub async fn hello_world_action(ctx: ActionContext, _n: ()) -> Result<(), TcpTar
 #[action_gen]
 pub async fn set_upstream_vault_action(
     ctx: ActionContext,
-    upstream: SocketAddr,
+    _upstream: SocketAddr,
 ) -> Result<(), TcpTargetError> {
+    // Ensure the instance is available
+    let Some(instance) = ctx.instance() else {
+        return Err(TcpTargetError::NotFound(
+            "Connection Instance Lost.".to_string(),
+        ));
+    };
+
+    if ctx.is_local() {
+        // Invoke on local
+        // Send the message to the server
+        let _ = instance.lock().await.write_text("Hello World!").await;
+    } else if ctx.is_remote() {
+        // Read the message from the client
+        let read = instance.lock().await.read_text().await?;
+        info!("{}", read)
+    }
+
     Ok(())
 }
--- crates/vcs_actions/src/registry/server_registry.rs
+++ crates/vcs_actions/src/registry/server_registry.rs
@@ -1,9 +1,9 @@
 use action_system::action_pool::ActionPool;
 
-use crate::actions::local_actions::register_hello_world_action;
+use crate::actions::local_actions::register_set_upstream_vault_action;
 
 pub fn server_action_pool() -> ActionPool {
     let mut pool = ActionPool::new();
-    register_hello_world_action(&mut pool);
+    register_set_upstream_vault_action(&mut pool);
     pool
 }
