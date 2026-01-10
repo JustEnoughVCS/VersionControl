a09d7538191846a0c0e86fcb87638e0bec182587
魏曹先生 <1992414357@qq.com>
2026-01-10 18:01:29 +0800

    Hide .cargo, .github and .temp directories before build

--- setup.ps1
+++ setup.ps1
@@ -7,3 +7,14 @@ if (Test-Path "docs/Documents/.git") {
 } else {
     git clone https://github.com/JustEnoughVCS/Documents.git docs/Documents
 }
+
+# Hide .cargo, .github and .temp directories before build
+if (Test-Path .cargo) {
+    attrib +h .cargo
+}
+if (Test-Path .github) {
+    attrib +h .github
+}
+if (Test-Path .temp) {
+    attrib +h .temp
+}
