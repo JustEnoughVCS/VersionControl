47e0ffd50427440696c245814517e4f5fa94ed83
魏曹先生 <1992414357@qq.com>
2026-03-08 22:48:54 +0800

    Move action system to legacy and remove storage system

--- systems/storage/src/error.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug, thiserror::Error)]
-pub enum StorageIOError {
-    #[error("IO error: {0}")]
-    IOErr(#[from] std::io::Error),
-
-    #[error("Hash too short")]
-    HashTooShort,
-}
